3fb68e2e9b213acc95cca9631f6f712fa38c9a78
NumberOneEngineer <mustafaesat01@gmail.com>
2024-09-24 23:04:08 +0300

    Refactor SVG rendering in StopAndWaitComponent, update frame rendering and corruption detection

--- src/app/data-link/stop-and-wait/stop-and-wait.component.ts
+++ src/app/data-link/stop-and-wait/stop-and-wait.component.ts
@@ -13,6 +13,7 @@ export type Frame = {
 };
 
 export type Process = {
+	id: number;
 	frame: Frame;
 	node: 'sender' | 'receiver';
 	sent: boolean;
@@ -29,19 +30,34 @@ export type Process = {
 	styleUrl: './stop-and-wait.component.scss',
 })
 export class StopAndWaitComponent {
+	senderFrames: Frame[] = [
+		{ id: 0, data: 'frame 0' },
+		{ id: 1, data: 'frame 1' },
+		{ id: 2, data: 'frame 2' },
+		{ id: 3, data: 'frame 3' },
+		{ id: 4, data: 'frame 4' },
+		{ id: 5, data: 'frame 5' },
+		{ id: 6, data: 'frame 6' },
+	];
+	receiverFrames: Frame[] = [];
+	senderAck = 0;
+	receiverAck = 0;
+
 	idTracker = 0;
 	timeTracker = signal(0);
-	lineNumber = computed(() => [].constructor(this.timeTracker()));
+	lineNumber = computed(() => [].constructor(this.timeTracker() + 1));
 
 	frameList = signal<Process[]>([]);
-	processList = computed(() => { });
 	frameSentIndex = 0;
 	svg = viewChild.required('svg', { read: ElementRef });
 	senderLine = viewChild.required('senderLine', { read: ElementRef });
 
 	dashedLines = viewChildren('dashedLine', { read: ElementRef });
-	dashedLineY = computed(() =>
-		this.dashedLines().map((line) => line.nativeElement.y1.baseVal.value) as number[]
+	dashedLineY = computed(
+		() =>
+			this.dashedLines().map(
+				(line) => line.nativeElement.y1.baseVal.value
+			) as number[]
 	);
 	setDashedLinePositions = computed(() => {
 		this.dashedLines().forEach((line, index) => {
@@ -64,46 +80,42 @@ export class StopAndWaitComponent {
 				const viewBox = this.svg()
 					.nativeElement.getAttribute('viewBox')
 					.split(' ');
-				viewBox[3] = `${Number(viewBox[3]) + 100}`;
+				viewBox[3] = `${Number(viewBox[3]) + this.frameList().length * 35}`;
 				this.svg().nativeElement.setAttribute('viewBox', viewBox.join(' '));
 			}
 		}
 	});
 
-	addFrame(frame: Omit<Frame, 'id'>) {
-		this.timeTracker.update((prev) => prev + 10);
+	addFrame(frame: Frame, node: 'sender' | 'receiver') {
+		this.timeTracker.update((prev) =>
+			node === 'sender' ? prev + 10 : prev + 6
+		);
 
 		this.frameList().push({
 			frame: {
-				id: this.idTracker++,
+				id: frame.id,
 				data: frame.data,
 			},
-			node: 'sender',
+			id: this.idTracker++,
+			node: node,
 			sent: true,
 			received: true,
 			startIndex: this.timeTracker() - 4,
 			endIndex: this.timeTracker(),
 		});
 
-		this.timeTracker.update((prev) => prev + 6);
-
-		this.frameList().push({
-			frame: {
-				id: this.idTracker++,
-				data: `ACK ${frame.data}`,
-			},
-			node: 'receiver',
-			sent: true,
-			received: true,
-			startIndex: this.timeTracker() - 4,
-			endIndex: this.timeTracker() - 1,
-		});
-
 		this.frameList.set([...this.frameList()]);
 	}
 
-	ngAfterViewInit() {
-		this.addFrame({ data: 'frame 0' });
+	ngOnInit() {
+		this.senderFrames.forEach((frame) => {
+			this.addFrame(frame, 'sender');
+			this.receiverFrames.push({ id: frame.id, data: `ACK ${frame.data}` });
+			this.addFrame(
+				this.receiverFrames[this.receiverFrames.length - 1],
+				'receiver'
+			);
+		});
 	}
 
 	modalVisible = false;
@@ -115,7 +127,60 @@ export class StopAndWaitComponent {
 	}
 
 	toggleReceived(): void {
-		this.frameList()[this.frameForModal!.frame.id].received =
-			!this.frameList()[this.frameForModal!.frame.id].received;
+		this.frameList()[this.frameForModal!.id].received =
+			!this.frameList()[this.frameForModal!.id].received;
+
+		let processCorrupted: Process = this.frameList()[0];
+		let processIndex = 0;
+
+		for (let i = 0; i < this.frameList().length; i++) {
+			if (this.frameList()[i] === this.frameForModal) {
+				processCorrupted = this.frameList()[i];
+				processIndex = i;
+				break;
+			}
+		}
+
+		this.frameList.set([...this.frameList().slice(0, processIndex + 1)]);
+		this.idTracker = processCorrupted.id + 1;
+
+		if (this.frameForModal?.node === 'sender') {
+			
+			this.receiverFrames.splice(
+				0,
+				processCorrupted.frame.id
+			);
+
+			this.timeTracker.set(processCorrupted.startIndex + (this.frameForModal.received ? 4 : 6));
+
+			if (this.frameForModal.received) {
+				this.receiverFrames.push({
+					id: this.senderFrames[processCorrupted.frame.id].id,
+					data: `ACK ${this.senderFrames[processCorrupted.frame.id].data}`,
+				});
+				this.addFrame(
+					this.receiverFrames[this.receiverFrames.length - 1],
+					'receiver'
+				);
+			} 
+
+			for (
+				let i = processCorrupted.frame.id + (this.frameForModal.received ? 1 : 0);
+				i < this.senderFrames.length;
+				i++
+			) {
+				this.addFrame(this.senderFrames[i], 'sender');
+				this.receiverFrames.push({
+					id: this.senderFrames[i].id,
+					data: `ACK ${this.senderFrames[i].data}`,
+				});
+				this.addFrame(
+					this.receiverFrames[this.receiverFrames.length - 1],
+					'receiver'
+				);
+			}
+		}
 	}
+
+	
 }
