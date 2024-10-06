002d6660a3acfea346d2b20a0c21f9cae90d1d9e
NumberOneEngineer <mustafaesat01@gmail.com>
2024-10-06 14:46:54 +0300

    Refactor styling for analog signal component

--- src/app/signal-function-visual/analog-signal/analog-signal.component.ts
+++ src/app/signal-function-visual/analog-signal/analog-signal.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+
+export type Sinus = {
+	amplitude: number;
+	frequency: number;
+	phaseDegree: number;
+};
 
 @Component({
-  selector: 'app-analog-signal',
-  standalone: true,
-  imports: [],
-  templateUrl: './analog-signal.component.html',
-  styleUrl: './analog-signal.component.scss'
+	selector: 'app-analog-signal',
+	standalone: true,
+	imports: [],
+	templateUrl: './analog-signal.component.html',
+	styleUrl: './analog-signal.component.scss',
 })
 export class AnalogSignalComponent {
+	sinus = input<Sinus>({
+		amplitude: 1,
+		frequency: 1,
+		phaseDegree: 0,
+	});
 
+	computeSinus = computed(() => {
+		const degreeToRad = this.sinus().phaseDegree / 360
+
+		const amplitudeHeight = (this.sinus().amplitude === 1 ? -49 : -50) + (( 1- this.sinus().amplitude) * 100);
+		const oscillationWidth = 100 / this.sinus().frequency;
+		const waveStartShifting = oscillationWidth * degreeToRad
+
+		let result = `M ${0 - waveStartShifting} 50 Q ${(oscillationWidth / 4) - waveStartShifting
+			} ${amplitudeHeight}, ${(oscillationWidth / 2) - waveStartShifting} 50 T ${(oscillationWidth / 2) + (oscillationWidth / 2) - waveStartShifting} 50`;
+
+		for (let i = 1; i < this.sinus().frequency * 2 + 2; i++) {
+			result += ` Q ${(oscillationWidth / 4) + (oscillationWidth * i) - waveStartShifting} ${amplitudeHeight}, ${(oscillationWidth / 2) + (oscillationWidth * i) - waveStartShifting} 50 T ${(oscillationWidth / 2) + (oscillationWidth * i) + (oscillationWidth / 2) - waveStartShifting} 50`;
+		}
+
+		return result;
+	});
 }
