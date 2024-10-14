80a397468ef14fc09eec9e343cab6bbb51c99f55
NumberOneEngineer <mustafaesat01@gmail.com>
2024-10-14 19:16:48 +0300

    Refactor drawSinus computation for analog signal component

--- src/app/signal-function-visual/analog-signal/analog-signal.component.ts
+++ src/app/signal-function-visual/analog-signal/analog-signal.component.ts
@@ -28,11 +28,11 @@ export class AnalogSignalComponent {
 
 	sinus = input<Sinus>({
 		amplitude: 1,
-		frequency: 1,
+		frequency: 6,
 		phase: 0
 	});
 
-	computeSinus = computed(() => {
+	/*computeSinus = computed(() => {
 		const degreeToRad = this.sinus().phase / 360
 
 		const amplitudeHeight = (this.sinus().amplitude === 1 ? -49 : -50) + ((1 - this.sinus().amplitude) * 100);
@@ -47,26 +47,56 @@ export class AnalogSignalComponent {
 		}
 
 		return result;
-	});
+	});*/
 
 	drawSinus = computed(() => {
+		const waveTopX = 25 / this.sinus().frequency;
+		const intervalScaling = (waveTopX * waveTopX) / 50
+		const intervalBetweenLines = waveTopX / 100
 		let result: LineCoordinates[] = [
 			{
 				x1: this.origin.x,
 				y1: this.origin.y,
 				x2: 0,
-				y2: 625 / 12.5
+				y2: (waveTopX * waveTopX) / intervalScaling
 			},
 		]
 
-		for (let i = 1; i <= 25; i++) {
+		for (let i = 0; i <= waveTopX * 2; i += intervalBetweenLines) {
 			result.push({
-				x1: result[i - 1].x2,
-				y1: result[i - 1].y2,
+				x1: result[result.length - 1].x2,
+				y1: result[result.length - 1].y2,
 				x2: i,
-				y2: ((25 - i) * (25 - i)) / 12.5
+				y2: ((waveTopX - i) * (waveTopX - i)) / intervalScaling
 			})
 		}
+
+		if (result.length === 201) {
+			result.push({
+				x1: result[result.length - 1].x2,
+				y1: result[result.length - 1].y2,
+				x2: result[result.length - 1].x2 + intervalBetweenLines,
+				y2: ((waveTopX - intervalBetweenLines) * (waveTopX - intervalBetweenLines)) / intervalScaling
+			})
+		}
+
+		for (let i = waveTopX * 2; i <= waveTopX * 4; i += intervalBetweenLines) {
+			result.push({
+				x1: result[result.length - 1].x2,
+				y1: result[result.length - 1].y2,
+				x2: i,
+				y2: (50 - (((waveTopX * 3) - i) * ((waveTopX * 3) - i)) / intervalScaling) + 50
+			})
+		}
+
+		result.push({
+			x1: result[result.length - 1].x2,
+			y1: result[result.length - 1].y2,
+			x2: result[result.length - 1].x2 + intervalBetweenLines,
+			y2: (50 - (((waveTopX * 3) - intervalBetweenLines) * ((waveTopX * 3) - intervalBetweenLines)) / intervalScaling) + 50
+		})
+
+		console.log(result)
 
 		return result;
 	});
