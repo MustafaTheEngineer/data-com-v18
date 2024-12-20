57312df50f0141ceb867ec29a015d2e588a91265
NumberOneEngineer <mustafaesat01@gmail.com>
2024-12-20 14:09:31 +0300

    Enhance PAM simulation component with responsive SVG rendering and dynamic axis scaling

--- src/app/signal-encoding/analog-to-digital/pam-simulation/pam-simulation.component.ts
+++ src/app/signal-encoding/analog-to-digital/pam-simulation/pam-simulation.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, HostListener, signal } from '@angular/core';
 
 @Component({
 	selector: 'app-pam-simulation',
@@ -9,5 +9,37 @@ import { Component, computed, signal } from '@angular/core';
 })
 export class PamSimulationComponent {
 	bitsPerPeriod = signal(5);
-	svgHeight = computed(() => 100 + 50 * this.bitsPerPeriod());
+	dataLevels = computed(() => 2 ** this.bitsPerPeriod());
+
+	svgWidth = signal(500);
+	svgHeight = signal(500);
+
+	coordXStart = 50
+	coordXEnd = computed(() => this.svgWidth() - this.coordXStart);
+
+	coordYStart = 50
+	coordYEnd = computed(() => this.svgHeight() - this.coordYStart);
+
+	ngAfterViewInit() {
+		this.onResize()
+	}
+
+	yAxisLevelCoords = computed(() => {
+		const result = [];
+
+		const interval = (this.coordYEnd() - this.coordXStart) / (this.dataLevels() - 1)
+
+		for (let i = 0; i < this.dataLevels(); i++) {
+			result.push(this.coordYStart + interval * i);
+		}
+
+		console.log(result);
+
+		return result;
+	});
+
+	onResize() {
+		this.svgWidth.set(window.innerWidth * 0.95);
+		this.svgHeight.set(window.innerHeight * 0.95);
+	}
 }
